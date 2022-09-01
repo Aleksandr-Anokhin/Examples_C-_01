92366be9288bc3d2deeab1fea0898a5dcbb33988
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2022-09-01 19:00:18 +0300

    Add Random()

--- Example_003_summ/Program.cs
+++ Example_003_summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int num_A = 57;
-int num_B = 75;
+﻿int num_A = new Random().Next(1, 10);  //Случайное число от 1 до 9
+Console.WriteLine(num_A);
+int num_B = new Random().Next(10, 20);
+Console.WriteLine(num_B);
 int result = num_A + num_B;
 Console.WriteLine(result);
